fc80ab4bebc415643d5cfb09f7fb371e8d2ac3a7
Sasmithaa V S <48088556+sasmithaasrinivasan@users.noreply.github.com>
2020-10-31 19:14:58 +0530

    Add files via upload

---
+++ FACULTY/JAVASCRIPT/studentforum.js
@@ -0,0 +1,19 @@
+function newque(){
+
+var newq = document.getElementById("nm");
+var viewq = document.getElementById("q1");
+newq.style.display = "block";
+viewq.style.display = "none";
+  
+}
+
+function viewque(){
+    var newq = document.getElementById("nm");
+    var viewq = document.getElementById("q1");
+    newq.style.display = "none";
+    viewq.style.display = "block";
+
+
+    
+      
+    }
